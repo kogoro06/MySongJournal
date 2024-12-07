4757be3124d89ac4ed32828fdeebbf67aad215f9
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-12-07 22:20:19 +0900

    日記タイトル入力フォーム作成

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,7 +1,56 @@
 class JournalsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_journal, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_journal, only: [:edit, :update, :destroy]
+
   def index
+    @journals = Journal.all
+  end
+
+  def show
+    @journals = Journal.find(params[:id])
   end
 
   def new
+    @journal = Journal.new
+  end
+
+  def create
+    @journal = current_user.journals.new(journal_params)
+    if @journal.save
+      redirect_to @journal, notice: "Journal was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @journal.update(journal_params)
+      redirect_to @journal, notice: "Journal was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @journal.destroy
+    redirect_to journals_path, notice: "Journal was successfully deleted."
+  end
+
+  private
+
+  def journal_params
+    params.require(:journal).permit(:title, :emotion, :content, :artist_name, :song_name, :preview_url, :album_image)
+  end
+
+  def set_journal
+    @journal = Journal.find(params[:id])
+  end
+
+  def authorize_journal
+    redirect_to journals_path, alert: "You are not authorized to perform this action." unless @journal.user == current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :journals, dependent: :destroy
   # Deviseのモジュール
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
