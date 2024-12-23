151660895b46ae10e54a3c73d9d39e0992dfeb8f
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-12-23 21:18:27 +0900

    lint修正

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,4 +1,4 @@
 # Spotify API認証をテスト環境では無効化
 unless Rails.env.test?
-  RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+  RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
 end
