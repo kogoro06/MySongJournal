ccd9841a9057517a634572fb0e4a5a5ac1c12467
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-12-23 18:26:25 +0900

    feat/sidekiq周り

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :journals
 
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
+
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
