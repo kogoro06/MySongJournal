bde691a232f21c6dcfac62e782cdcf716b448573
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-12-23 20:46:28 +0900

    test環境に.envの内容反映

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+  ENV["SPOTIFY_CLIENT_ID"] ||= "a6bb07593cdd421899b12e3661b3bc1b"
+  ENV["SPOTIFY_CLIENT_SECRET"] ||= "42f4957c2880402880ca1a86115c842a"
 end
