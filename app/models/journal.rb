3c1e5b502c7de8a4d49527df7667ff81321deb30
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2024-12-23 18:45:10 +0900

    ジャーナルの改善

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -3,4 +3,9 @@ class Journal < ApplicationRecord
   enum emotion: { 喜: 0, 怒: 1, 哀: 2, 楽: 3 }
   validates :title, presence: true, length: { maximum: 50 } # 必須、最大50文字
   validates :content, presence: true, length: { maximum: 500 } # 必須、最大500文字
+  validates :emotion, presence: true # 必須
+  validates :song_name, presence: true, length: { maximum: 100 }
+  validates :artist_name, presence: true, length: { maximum: 100 }
+  validates :album_image, presence: true, format: { with: URI.regexp(%w[http https]), message: "must be a valid URL" }
+  validates :previewURL, presence: true, format: { with: URI.regexp(%w[http https]), message: "must be a valid URL", allow_nil: true }
 end
