3e5e6bbb45412ac56b1b7445a4263fe2434f6b8c
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-01-06 02:25:43 +0900

    アルバム名をキーワードに変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import { initializeButtonsAndModal } from "./controllers/modal";
 import "./controllers/search";
-import "./controllers/search_conditions";
 
 document.addEventListener('turbo:load', initializeButtonsAndModal);
 document.addEventListener('turbo:render', initializeButtonsAndModal);
