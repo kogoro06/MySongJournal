a3ef3f37cda988d8f895c8757646f57677f1b8e6
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-01-14 22:25:58 +0900

    lint修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "others_journal/index"
   devise_for :users
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # 日記関連のルート
   resources :journals
-  
+
   require "sidekiq/web"
   mount Sidekiq::Web => "/sidekiq"
 
