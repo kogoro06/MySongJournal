f40f522f8bd384089dedac266a813a37f434effb
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-01-31 00:24:38 +0900

    フォロー・フォロワー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,25 @@ class User < ApplicationRecord
   has_many :liked_journals, through: :likes, source: :journal
   has_many :favorites, dependent: :destroy
   has_many :favorited_journals, through: :favorites, source: :journal
+
+  # フォローしている関連
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+
+  # フォローされている関連
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower
+
+  # フォロー関連のメソッド
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_30_143406) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_30_150154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_30_143406) do
     t.index ["journal_id"], name: "index_favorites_on_journal_id"
     t.index ["user_id", "journal_id"], name: "index_favorites_on_user_id_and_journal_id", unique: true
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "journals", force: :cascade do |t|
