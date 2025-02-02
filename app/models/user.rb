9933315ff5577a1fa0009acd505a159598d7dfa1
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-03 00:14:52 +0900

    user.rb のきさい

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,12 @@ class User < ApplicationRecord
   has_one :profile
   # Deviseのモジュール
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   # バリデーション
   validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   # favorites の関連
   has_many :favorites, dependent: :destroy
@@ -32,5 +35,16 @@ class User < ApplicationRecord
 
   def following?(other_user)
     following.include?(other_user)
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+  def self.create_unique_string
+    SecureRandom.uuid
   end
 end
