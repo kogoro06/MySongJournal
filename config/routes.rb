ae61699d561fe274dc1cd24fde68306c85c1bf6a
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-03 00:11:28 +0900

    routes.rb にルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get "other_users/show"
   get "otherusers/show"
   get "others_journal/index"
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
