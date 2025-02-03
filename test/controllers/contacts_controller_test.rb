240ffc56cdfe36c826168ee124675cf39b6e1cb6
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-03 13:56:20 +0900

    test修正

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,12 +2,20 @@ require "test_helper"
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get contacts_new_url
+    get new_contact_url
     assert_response :success
   end
 
   test "should get create" do
-    get contacts_create_url
-    assert_response :success
+    assert_difference("Contact.count") do
+      post contacts_url, params: { 
+        contact: { 
+          name: "Test User",
+          email: "test@example.com",
+          message: "This is a test message"
+        }
+      }
+    end
+    assert_redirected_to root_url
   end
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -2,10 +2,16 @@ require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
   test "notification" do
-    mail = ContactMailer.notification
-    assert_equal "Notification", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    contact = Contact.new(
+      name: "Test User",
+      email: "test@example.com",
+      message: "This is a test message"
+    )
+    
+    mail = ContactMailer.notification(contact)
+    assert_equal "【MySongJournal】新しいお問い合わせがありました", mail.subject
+    assert_equal ["mysongjournalconfirmable@gmail.com"], mail.to
+    assert_equal ["mysongjournalconfirmable@gmail.com"], mail.from
+    assert_equal ["test@example.com"], mail.reply_to
   end
 end
