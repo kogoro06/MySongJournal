a4e05f15692f3e1cc23906d2217ee19fcf36dd89
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-03 17:11:55 +0900

    xss 修正

--- app/controllers/other_users_controller.rb
+++ app/controllers/other_users_controller.rb
@@ -11,4 +11,15 @@ class OtherUsersController < ApplicationController
     @journals = @user.journals.order(created_at: :desc).page(params[:page]).per(3)
     @liked_journals = @user.liked_journals.order(created_at: :desc).page(params[:page]).per(3)
   end
+
+  def x_redirect
+    user = User.find(params[:id])
+    x_link = user.safe_x_link
+    
+    if x_link.present?
+      redirect_to x_link, allow_other_host: true
+    else
+      redirect_to user_path(user), alert: 'Xのリンクが登録されていません'
+    end
+  end
 end
