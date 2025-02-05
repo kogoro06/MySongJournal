4146e213c509e419eb88a0419aba1aa6b5f3ef8d
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-05 18:20:33 +0900

    リトライ設定の修正

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -3,7 +3,4 @@ require "openai"
 OpenAI.configure do |config|
   config.access_token = ENV["OPENAI_API_KEY"]
   config.request_timeout = 240
-  # OpenAIの初期化設定を追加
-  config.max_retries = 3
-  config.retry_interval = 1
 end
