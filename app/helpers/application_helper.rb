ad8233f02a0da6d3e81f41704237cb8728a4e487
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-07 03:37:34 +0900

    修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,22 @@
 module ApplicationHelper
   def default_meta_tags
     {
+      site: "MY SONG JOURNAL",
+      title: "",
+      reverse: true,
+      separator: "|",
+      description: "音楽と一緒に日々の思い出を記録しよう",
+      keywords: "日記,音楽,Spotify",
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
       og: {
-        title: "MY SONG JOURNAL",
         site_name: "MY SONG JOURNAL",
-        type: "website"
+        title: "",
+        description: "音楽と一緒に日々の思い出を記録しよう",
+        type: "website",
+        url: request.original_url,
+        image: image_url("ogp.png").to_s,
+        locale: "ja_JP"
       },
       twitter: {
         card: "summary_large_image"
