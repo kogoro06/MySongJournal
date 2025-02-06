b3f8beda026a7ed44e5111c663c692d4a50fecff
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-07 05:48:00 +0900

    実験

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -51,7 +51,7 @@ class JournalsController < ApplicationController
     @user_name = @user.name
 
     # OGP用の変数を設定
-    @ogp_title = @journal.song_name || "MY SONG JOURNAL"
+    @ogp_title ="MY SONG JOURNAL"
     @ogp_description = @journal.artist_name || "音楽と一緒に日々の思い出を記録しよう"
     @ogp_image = if @journal.album_image.present?
       "#{request.base_url}/images/ogp.png?album_image=#{ERB::Util.url_encode(@journal.album_image)}"
