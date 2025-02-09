1b4bad0ca9962fc1ff5aa4f50c7c093d1e7a6ebe
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-10 04:14:44 +0900

    uidが消えてしまっていたので追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_05_080133) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_190930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -131,8 +131,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_05_080133) do
     t.datetime "updated_at", null: false
     t.text "bio"
     t.string "x_link"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
     t.index ["x_link"], name: "index_users_on_x_link", unique: true, where: "(x_link IS NOT NULL)"
   end
 
