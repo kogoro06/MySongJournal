0dacd5bf810bbd22984d32d8610a9930118e8209
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-09 23:34:29 +0900

    friendly_idのslug修正

--- app/controllers/concerns/ogp/meta_tags_handler.rb
+++ app/controllers/concerns/ogp/meta_tags_handler.rb
@@ -10,17 +10,19 @@ module Ogp
       @ogp_title = @journal.song_name.presence || "MY SONG JOURNAL"
       @ogp_description = @journal.artist_name.presence || "音楽と一緒に日々の思い出を記録しよう"
       @ogp_image = generate_ogp_image_url(@journal)
+      @ogp_url = journal_url(@journal)
     end
 
     def generate_ogp_image_url(journal)
-      cache_key = journal.updated_at.to_i.to_s
+      cache_key = "#{journal.id}-#{journal.updated_at.to_i}"
 
       url_for(
         controller: :images,
         action: :ogp,
         text: "#{journal.song_name} - #{journal.artist_name}",
         album_image: journal.album_image,
-        v: cache_key
+        v: cache_key,
+        journal_id: journal.id
       )
     end
   end
