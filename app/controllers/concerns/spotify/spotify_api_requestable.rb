154878f32a224a22acb5033b90403428b535541c
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-10 21:02:59 +0900

    spotify_api_requestable.rbの最適化

--- app/controllers/concerns/spotify/spotify_api_requestable.rb
+++ app/controllers/concerns/spotify/spotify_api_requestable.rb
@@ -1,8 +1,9 @@
 module Spotify::SpotifyApiRequestable
   extend ActiveSupport::Concern
-  require "rest-client"
 
   private
+
+  MAX_ATTEMPTS = 3
 
   def spotify_get(endpoint, params = {})
     token = get_spotify_access_token
@@ -12,12 +13,7 @@ module Spotify::SpotifyApiRequestable
     )
     JSON.parse(response.body)
   rescue RestClient::Unauthorized
-    token = get_spotify_access_token
-    response = RestClient.get(
-      "https://api.spotify.com/v1/#{endpoint}",
-      default_headers(token).merge(params)
-    )
-    JSON.parse(response.body)
+    handle_unauthorized_access
   end
 
   def default_headers(token)
@@ -25,5 +21,15 @@ module Spotify::SpotifyApiRequestable
       Authorization: "Bearer #{token}",
       "Accept-Language" => "ja"
     }
+  end
+
+  def handle_unauthorized_access
+    @attempts ||= 0
+    @attempts += 1
+    if @attempts < MAX_ATTEMPTS
+      spotify_get(endpoint, params)
+    else
+      raise "Unauthorized access after multiple attempts"
+    end
   end
 end
