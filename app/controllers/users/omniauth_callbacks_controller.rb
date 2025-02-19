90af1e7cddd0372401e7491a4f176be12575507f
kogoro06 <166668414+kogoro06@users.noreply.github.com>
2025-02-19 20:09:10 +0900

    google-oathログイン時のメッセージ変更（要テスト）

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: [ :google_oauth2, :spotify ]
 
   def google_oauth2
-    callback_for(:google_oauth2)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, scope: [:devise, :omniauth_callbacks, :google_oauth2]) if is_navigational_format?
+    else
+      Rails.logger.error "OAuth user creation failed: #{@user.errors.full_messages.join(', ')}"
+      set_flash_message(:alert, :failure, scope: [:devise, :omniauth_callbacks, :google_oauth2])
+      redirect_to new_user_registration_url
+    end
+  rescue => e
+    Rails.logger.error "OAuth error: #{e.message}"
+    redirect_to new_user_registration_url
   end
 
   # 将来的な機能拡張時に使用
